f0ac0a2837f76965a40b8c494a4108f7fb765757
Wasiu Bakare <wasiubakare@gmail.com>
2025-03-20 02:57:08 +0100

    refactor: integration.json to use dynamic route

--- src/common/middlewares/error.handler.ts
+++ src/common/middlewares/error.handler.ts
@@ -1,14 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 import { configService } from "../../config";
 
-export const globalErrorHandler = (err: Error, req: Request, res: Response) => {
+export const globalErrorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   res.status(500).json({
     success: false,
     status: 500,
     message: err.message || "Internal Server Error!",
     stack: configService.NODE_ENV === "development" ? err.stack : undefined,
   });
-
-  return;
 };
--- src/routes.ts
+++ src/routes.ts
@@ -17,6 +17,14 @@ const setupRoutes = (app: Application): void => {
     res.json(telexService.getTelexIntegrationConfig());
     return;
   });
+
+  app.post(
+    "/webhook",
+    catchAsync(async (req: Request, res: Response) => {
+      console.log(req.body);
+      res.status(200).json({});
+    }),
+  );
 };
 
 export default setupRoutes;
